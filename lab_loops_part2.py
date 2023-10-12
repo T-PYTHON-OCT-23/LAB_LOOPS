4a5a47e4e92a36d2af328c1b5d96e116f44cb46d
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-10-12 13:30:52 +0300

    The lab loop is solved-part(2)

---
+++ lab_loops_part2.py
@@ -0,0 +1,6 @@
+
+result = "What is the product of this equation ( 7*24 ):"
+while int(input(result)) != 168:
+    print("Your answer is wrong try again! ")
+else:
+    print("Your answered this Question correctly")
