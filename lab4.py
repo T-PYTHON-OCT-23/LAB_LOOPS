78cedf6a2d383292bdd0926fe7b2e5bfe69a5029
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-10-12 13:43:35 +0300

    Done

---
+++ lab4.py
@@ -0,0 +1,16 @@
+for num in range(45,210):
+    if num==100:
+        continue
+    if num==205:
+        break
+    print(num)
+
+
+while int(input("what is the product of 7 * 24 ?"))!=168:
+    print('"Your Answer is wrong try again.."')
+
+else:
+    print("You answered this Question correctly")
+    
+
+   
