62f0f2203a529eded8c8f41398ae6b46265e1baa
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-10-12 14:37:20 +0300

    Bouns

---
+++ B.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the positive number: "))
+sum=0
+for i in range(1,num+1):
+        if i%2==0:
+            sum+=i           
+print("The sum of even numbers between 1 and ",num,"is",sum)
+
+
+
