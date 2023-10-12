ab2c0678efc4b5bdc23bf400c8718b103d2efe3a
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-10-12 14:06:20 +0300

    done!!!

--- loops.py
+++ loops.py
@@ -1,4 +1,5 @@
 
+#lab1
 i = range(45,210)
 for n in i:
     if n==100:
@@ -6,6 +7,14 @@ for n in i:
     if n==205 :
      break
     print(n)
+
+#lab2
+Q="what is the product of 7 * 24 ?"
+while input(Q)!= "168":
+    print("your Answer is wrong try again..")
+else:
+    print("You answered this Question correctly")
+  
     
   
 
