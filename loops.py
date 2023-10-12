ef165b5b3eed9f0f0e43001e266ffdaae5a1a49b
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-10-12 13:54:01 +0300

    done!

--- loops.py
+++ loops.py
@@ -1,11 +1,11 @@
 
 i = range(45,210)
 for n in i:
-    if n == 100:
-        continue
-    print(n)
-    while i==205 :
+    if n==100:
+     continue
+    if i==205 :
      break
+    print(n)
     
   
 
