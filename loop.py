961fd991df870c8c491c3261663febb3f2da127e
Hadeel-7i <147502790+Hadeel-7i@users.noreply.github.com>
2023-10-20 19:03:15 +0300

    Update loop.py

--- loop.py
+++ loop.py
@@ -10,9 +10,13 @@ for r in iterate:
 
 #(2)using while loop
 Question = "Find the result of multiplying 7 by 4"
-answer = 28
+trials = 10 
 
-while input(Question) != "answer":
-        print("That's wrong Try again..")
+while input(Question) != str(28): 
+    trials -= 1
+    if trials <= 0: 
+        print("You used all your tries! the answer is 28.")
+        break
+    print(f"That's wrong Try again.. {trials} left ")
 else:
     print("Great! You answered this Question correctly")
